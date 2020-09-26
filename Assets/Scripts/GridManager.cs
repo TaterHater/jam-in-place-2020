9397208c5d30adc8da8a50e67479cc6ab0da00a4
larsbrady <47048242+larsbrady@users.noreply.github.com>
2020-09-26 00:30:52 -0400

    Spawns a grid of farm plots
    Grid is customizeable. You can change number of rows/columns.

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+
+    public float x_Start, z_Start;
+
+    public int numberOfColumns, numberOfRows;
+
+    public float x_Space, z_Space, y_Space;
+    public GameObject prefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numberOfColumns * numberOfRows; i++)
+        {
+            Instantiate(prefab, new Vector3(x_Start + (x_Space * (i % numberOfColumns)), y_Space, z_Start + (z_Space * (i / numberOfColumns))), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
