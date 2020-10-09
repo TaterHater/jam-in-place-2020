a3bacc6760b68a5b8218a00baeb8e5504b004443
curahee24 <curahee24@gmail.com>
2020-10-09 13:44:53 -0600

    fixed movement

--- Assets/Scripts/CharacterControllers/targetmove.cs
+++ Assets/Scripts/CharacterControllers/targetmove.cs
@@ -1,36 +1,42 @@
-﻿ using UnityEngine;
- using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System;
 
 public class targetmove : MonoBehaviour
- {
-     Vector3 newPosition;
+{
+    Vector3 newPosition;
     InteractableObject interactableObj = null;
-    
-     void Start () {
-         newPosition = transform.position;
-     }
-     void Update()
-     {
-         if (Input.GetMouseButtonDown(1))
-         {
-             RaycastHit hit;
-             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-             if (Physics.Raycast(ray, out hit))
-             {
-                 newPosition = hit.point;
-                 newPosition.y = -0.442f;
+
+    void Start()
+    {
+        newPosition = transform.position;
+    }
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Debug.Log(newPosition.x + " " + newPosition.y);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                newPosition = hit.point;
+                newPosition.y = -0.358f;
+
                 interactableObj = hit.collider.gameObject.GetComponent<InteractableObject>();
-             }
-         }
-     }
-     public Vector3 GetPosition(){
-         return newPosition;
-     }
+            }
+        }
+    }
+    public Vector3 GetPosition()
+    {
+        return newPosition;
+    }
 
     internal void Interact(PlayerScript ps)
     {
-        if (interactableObj != null) {
+        if (interactableObj != null)
+        {
             interactableObj.Interact(ps);
             interactableObj = null;
         }
